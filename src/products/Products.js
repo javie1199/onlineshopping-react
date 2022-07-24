44456350d424c9c6207cd6a764e6107045daae33
Jon Tran <trungt8998@gmail.com>
2022-07-24 07:40:35 -0500

    Add Products and attach its style

---
+++ src/products/Products.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Product from "./Product.js";
+import useFetch from "../useFetch.js";
+import Loader from "../Loader";
+import "./Products.Style.css";
+
+export default function Products(props) {
+  const [products, setProducts] = useState([]);
+  const { get, loading } = useFetch(
+    "https://react-tutorial-demo.firebaseio.com/"
+  );
+
+  useEffect(() => {
+    get("supermarket.json")
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => console.log("Could not load products", error));
+  }, []);
+
+  return (
+    <div className="products-layout">
+      <h1>Products</h1>
+      <p>Take a look at our products</p>
+      <div className="products-grid">
+        {loading && <Loader />}
+        {products.map((product) => {
+          return (
+            <Product
+              key={product.id}
+              details={product}
+              cart={props.cart}
+              onProductAdd={props.onProductAdd}
+              onProductDelete={props.onProductDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
