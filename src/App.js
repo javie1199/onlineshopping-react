cbf88996f1ec16cb1b28ef6d919e338fae19cd96
Jon Tran <trungt8998@gmail.com>
2022-07-23 23:08:09 -0500

    Add About component and its style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Home from "./Home";
+import About from "./About";
 import Navbar from "./Navbar";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
-      <Home />
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 };
