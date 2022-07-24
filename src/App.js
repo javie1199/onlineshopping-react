66f25ed358fb02120df290ee8ada8023021e9067
Jon Tran <trungt8998@gmail.com>
2022-07-24 07:43:11 -0500

    Update App component with Products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Home from "./Home";
 import About from "./About";
 import Navbar from "./Navbar";
+import Products from "./products/Products";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
+  const [cart, setCart] = useState(function() {
+    let savedCart = [];
+    try {
+      savedCart = JSON.parse(localStorage.getItem("cart")) || [];
+    } catch (error) {
+      savedCart = [];
+    }
+    return savedCart;
+  });
+
+  useEffect(() => {
+    if (cart) {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
+  function handleProductDelete(id) {
+    const updatedCart = cart.filter((product) => product.id !== id);
+    setCart(updatedCart);
+  }
+
+  function handleProductAdd(newProduct) {
+    // check if item exists
+    const existingProduct = cart.find(
+      (product) => product.id === newProduct.id
+    );
+    if (existingProduct) {
+      // increase quantity
+      const updatedCart = cart.map((product) => {
+        if (product.id === newProduct.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1,
+          };
+        }
+        return product;
+      });
+      setCart(updatedCart);
+    } else {
+      // product is new to the cart
+      setCart([
+        ...cart,
+        {
+          ...newProduct,
+          quantity: 1,
+        },
+      ]);
+    }
+  }
   return (
     <BrowserRouter>
       <Navbar />
@@ -15,6 +65,13 @@ const App = () => {
           </Route>
           <Route exact path="/about">
             <About />
+          </Route>
+          <Route exact path="/products">
+            <Products
+              cart={cart}
+              onProductAdd={handleProductAdd}
+              onProductDelete={handleProductDelete}
+            />
           </Route>
         </Switch>
       </div>
